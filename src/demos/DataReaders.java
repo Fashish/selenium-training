26b91360a73feceb58c5e6d01a028093050cbd0a
Fashish <fashish@gmail.com>
2018-07-20 15:32:06 +0100

    Add DDT related test files

---
+++ src/demos/DataReaders.java
@@ -0,0 +1,34 @@
+package demos;
+
+import java.util.List;
+
+public class DataReaders {
+
+	public static void main(String[] args) {
+		readXLS();
+		
+
+	}
+
+	public static void readCSV() {
+		String filename = "/Users/fash/Dev/eclipse-workspace/SeleniumTraining/files/UserAccounts.csv";
+		List <String[]> records = utilities.CSV.get(filename);
+		
+		for (String[] record : records) {
+			for (String field : record) {
+				System.out.println(field);
+			}
+		}
+	}
+	
+	public static void readXLS() {
+		String filename = "/Users/fash/Dev/eclipse-workspace/SeleniumTraining/files/UserLogin.xls";
+		String[][] data = utilities.Excel.get(filename);
+		for (String[] record : data) {
+			System.out.println("\nNEW RECORD");
+			System.out.println(record[0]);
+			System.out.println(record[1]);
+			System.out.println(record[2]);
+		}
+	}
+}
